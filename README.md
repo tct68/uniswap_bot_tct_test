e5447f0595e571c7e68349d9c6a925479175a28a
Thuong Truong <thuong@gmail.com>
2024-03-02 13:16:58 +0700

    Add file

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Uniswap Bot test" 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { provider, poolContract } from "uniswap_bot_tct/src/index";
+import main from "teaint_div";
+export {
+    provider,
+    poolContract,
+    main
+}
